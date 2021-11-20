43fd6448a9a905f14f8d8c251ad9757ea262c347
steltii <94451372+steltii@users.noreply.github.com>
2021-11-20 15:38:40 +0200

    Update lab1_2.ino

--- lab1/lab1_2.ino
+++ lab1/lab1_2.ino
@@ -1,5 +1,5 @@
 void setup() {
-  pinMode(2, OUTPUT);   
+  pinMode(2, OUTPUT);              //Set output pins
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
@@ -10,15 +10,15 @@ void setup() {
 
 void loop() {
   
-//0
+//0                               //Writers number 0 on display
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(7, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(2, LOW);   
       digitalWrite(3, LOW);   
       digitalWrite(4, LOW);   
@@ -27,23 +27,24 @@ void loop() {
       digitalWrite(7, LOW);        
       delay(100);
 
-//1
+//1                               //Writers number 1 on display
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
       delay(100);
-//2
+  
+//2                               //Writers number 2 on display
       digitalWrite(4, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(8, HIGH);
       digitalWrite(7, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(9, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(4, LOW);
       digitalWrite(3, LOW);
       digitalWrite(8, LOW);
@@ -51,15 +52,16 @@ void loop() {
       digitalWrite(6, LOW);
       digitalWrite(9, LOW);       
       delay(100);
-//3
+  
+//3                               //Writers number 3 on display
       digitalWrite(3, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(9, HIGH);
       digitalWrite(8, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(3, LOW);
       digitalWrite(6, LOW);
       digitalWrite(4, LOW);
@@ -67,15 +69,16 @@ void loop() {
       digitalWrite(9, LOW);
       digitalWrite(8, LOW);        
       delay(100);
-//4
+  
+//4                               //Writers number 4 on display
       digitalWrite(2, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(8, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(9, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(2, LOW);
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
@@ -83,15 +86,16 @@ void loop() {
       digitalWrite(5, LOW);
       digitalWrite(9, LOW);       
       delay(100);
-//5
+  
+//5                               //Writers number 5 on display
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(8, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
       digitalWrite(9, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(2, LOW);
       digitalWrite(3, LOW);
       digitalWrite(8, LOW);
@@ -99,7 +103,8 @@ void loop() {
       digitalWrite(6, LOW);
       digitalWrite(9, LOW);        
       delay(100);
-//6
+  
+//6                               //Writers number 6 on display
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(5, HIGH);
@@ -107,8 +112,8 @@ void loop() {
       digitalWrite(7, HIGH);
       digitalWrite(9, HIGH);
       digitalWrite(8, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(2, LOW);
       digitalWrite(3, LOW);
       digitalWrite(5, LOW);
@@ -117,19 +122,21 @@ void loop() {
       digitalWrite(9, LOW);
       digitalWrite(8, LOW);       
       delay(100);
- //7
+  
+ //7                               //Writers number 7 on display
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(9, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
       digitalWrite(9, LOW);      
       delay(100);
-//8
+  
+//8                               //Writers number 8 on display
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
@@ -138,8 +145,8 @@ void loop() {
       digitalWrite(7, HIGH);
       digitalWrite(8, HIGH);
       digitalWrite(9, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(2, LOW);
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
@@ -149,7 +156,8 @@ void loop() {
       digitalWrite(8, LOW);
       digitalWrite(9, LOW);       
       delay(100);
-//9
+  
+//9                               //Writers number 9 on display
       digitalWrite(2, HIGH);
       digitalWrite(3, HIGH);
       digitalWrite(4, HIGH);
@@ -157,8 +165,8 @@ void loop() {
       digitalWrite(6, HIGH);
       digitalWrite(8, HIGH);
       digitalWrite(9, HIGH);
-      delay(100);
-  //reset
+      delay(100);                  //Delay for how long number is shown
+  //reset                          //Turns display off
       digitalWrite(2, LOW);
       digitalWrite(3, LOW);
       digitalWrite(4, LOW);
